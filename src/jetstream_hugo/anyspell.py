a30b3b3f8d451e5f9df0a851b616eae2551fa826
hbanderi <hugo.banderier@unibe.ch>
2025-02-17 15:18:06 +0000

    moved som pers spell from xpysom, belongs here

--- src/jetstream_hugo/anyspell.py
+++ src/jetstream_hugo/anyspell.py
@@ -174,6 +174,83 @@ def mask_from_spells(
     return ds_masked
 
 
+def get_spells_sigma(df: pl.DataFrame, dists: np.ndarray, sigma: int = 1) -> pl.DataFrame:
+    start = 0
+    spells = []
+    while True:
+        start_lab = df[int(start), "labels"]
+        next_distance_cond = dists[start_lab, df[start:, "labels"]] > sigma
+        if not any(next_distance_cond):
+            spells.append({"rel_start": start, "value": start_lab, "len": len(df[start:, "labels"])})
+            break
+        to_next = np.argmax(next_distance_cond)
+        val = df[int(start): int(start + to_next), ["labels"]].with_columns(pl.col("labels").drop_nulls().mode().first().alias("mode"))[0, "mode"]
+        spells.append({"rel_start": start, "value": val, "len": to_next})
+        start = start + to_next
+    return pl.DataFrame(spells).with_columns(year=df[0, "year"], my_len=df.shape[0])
+
+
+def get_persistent_spell_times_from_som(
+    labels, dists: np.ndarray, sigma: int = 0, minlen: int = 4, nt_before: int = 0, nt_after: int = 0, nojune: bool = True, daily: bool = False,
+):
+    labels_df = xarray_to_polars(labels)
+    labels_df = labels_df.with_columns(pl.col("time").dt.year().alias("year"))
+    index_columns = get_index_columns(labels_df)
+    index = labels_df[index_columns].unique(maintain_order=True)
+
+    out = labels_df.group_by("year", maintain_order=True).map_groups(partial(get_spells_sigma, dists=dists, sigma=sigma))
+    out = out.with_columns(start = pl.col("year").rle_id() * pl.col("my_len") + pl.col("rel_start"))
+    out = out.with_columns(
+        range=pl.int_ranges(
+            pl.col("start") - nt_before, pl.col("start") + pl.col("len") + nt_after
+        ),
+        relative_index=pl.int_ranges(-nt_before, pl.col("len") + nt_after, dtype=pl.Int16),
+    )
+    out = out.with_row_index("spell").explode(["range", "relative_index"])
+    out = out.filter(pl.col("range") < len(index), pl.col("range") >= 0)
+    out = out.with_columns(index[out["range"]])
+    out = out.filter(pl.col("len") >= minlen)
+    out = out.with_columns(pl.col("spell").rle_id())
+    out = (
+        out.group_by("spell", maintain_order=True)
+        .agg(
+            [
+                pl.col(col).filter(
+                    pl.col("time").dt.year()
+                    == pl.col("time")
+                    .dt.year()
+                    .get(pl.arg_where(pl.col("relative_index") == 0).first())
+                )
+                for col in ["time", "relative_index", "value", "len"]
+            ]
+        )
+        .explode(["time", "relative_index", "value", "len"])
+    )
+    if not nojune:
+        return out
+    june_filter = out.group_by("spell", maintain_order=True).agg(
+        (pl.col("time").dt.ordinal_day() <= 160).sum() > 0.8
+    )["time"]
+    out = out.filter(pl.col("spell").is_in(june_filter.not_().arg_true()))
+    out = out.with_columns(pl.col("spell").rle_id())
+    out = out.with_columns(
+        out.group_by("spell", maintain_order=True)
+        .agg(
+            relative_time=pl.col("time")
+            - pl.col("time").get(pl.arg_where(pl.col("relative_index") == 0).first())
+        )
+        .explode("relative_time")
+    )
+    if not daily:
+        return out
+    
+    ratio = out.filter(pl.col("relative_index") == 1)[0, "relative_time"] / datetime.timedelta(days=1)
+    out = out.with_columns(pl.col("time").dt.round("1d")).unique(["spell", "time"], maintain_order=True)
+    out = out.with_columns(out.group_by("spell", maintain_order=True).agg(pl.col("relative_index").rle_id() + (pl.col("relative_index").first() * ratio).round().cast(pl.Int16)).explode("relative_index"))
+    out = out.with_columns(relative_time=pl.col("relative_index") * pl.duration(days=1))
+    return out
+
+
 def get_persistent_jet_spells(props_as_df, metric: Literal["persistence", "com_speed"], jet: Literal["EDJ", "STJ"], season: list | str | None = None, q: float = 0.2, daily: bool = False):
     props_as_df = extract_season_from_df(props_as_df, season)
     times = props_as_df["time"].unique()
