cc3c39fa9492ec7be3b4b234b221bfa436d21b28
Hugo <hugo.banderier@giub.unibe.ch>
2025-03-25 12:07:11 +0100

    vs

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# import sphinx_rtd_theme
+import jetstream_hugo
 
 
 # -- Project information -----------------------------------------------------
@@ -21,14 +24,22 @@ project = 'jet_stream'
 copyright = '2025, Hugo Banderier'
 author = 'Hugo Banderier'
 
+# The version info for the project you're documenting, acts as replacement
+# for |version| and |release|, also used in various other places throughout
+# the built documents.
+#
+# The short X.Y version.
+version = jetstream_hugo.__version__
+# The full version, including alpha/beta/rc tags.
+release = jetstream_hugo.__version__
+
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -36,6 +47,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+include_patterns = []
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
