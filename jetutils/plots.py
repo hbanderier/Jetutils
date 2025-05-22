80c1667c24224ba258b89d11606e8203391f576c
hbanderi <hugo.banderier@giub.unibe.ch>
2025-05-22 16:42:55 +0200

    added dn to gather

--- jetutils/plots.py
+++ jetutils/plots.py
@@ -1219,9 +1219,11 @@ def _gather_normal_da_jets_wrapper(
     da: xr.DataArray,
     n_interp: int = 30,
     clim: xr.DataArray | None = None,
+    half_length: float = 20,
+    dn: float = 1,
 ):
     jets = times.join(jets, on="time", how="left")
-    jets = gather_normal_da_jets(jets, da, half_length=20, dn=1)
+    jets = gather_normal_da_jets(jets, da, half_length=half_length, dn=dn)
     varname = da.name + "_interp"
 
     jets = interp_jets_to_zero_one(jets, [varname, "is_polar"], n_interp=n_interp)
@@ -1245,6 +1247,8 @@ def gather_normal_da_jets_wrapper(
     da: xr.DataArray,
     n_interp: int = 30,
     n_bootstraps: int = 0,
+    half_length: float = 20,
+    dn: float = 1,
     clim: xr.DataArray | None = None,
     time_before: datetime.timedelta = datetime.timedelta(0),
     time_after: datetime.timedelta = datetime.timedelta(0),
@@ -1252,7 +1256,7 @@ def gather_normal_da_jets_wrapper(
     times = extend_spells(times, time_before=time_before, time_after=time_after)
     varname = da.name + "_interp"
     if not n_bootstraps:
-        jets = _gather_normal_da_jets_wrapper(jets, times, da, n_interp, clim=clim)
+        jets = _gather_normal_da_jets_wrapper(jets, times, da, n_interp, clim=clim, half_length=half_length, dn=dn)
         jets = jets.group_by(
             [pl.col("is_polar") > 0.5, "norm_index", "n"], maintain_order=True
         ).agg(pl.col(varname).mean())
