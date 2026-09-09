0c525501455e0ae57cc6d82b3510e8b6add781f1
Hugo <hugo.banderier@giub.unibe.ch>
2026-09-09 20:50:32 +0200

    trying to fix ubelix bug, wasn't my fault actually

--- scripts/Jupyter/launch_jupyter.sh
+++ scripts/Jupyter/launch_jupyter.sh
@@ -1,2 +1,6 @@
 #/bin/bash!
-./jupyter-compute 16805 --jupyter_args="--ServerApp.allow_origin=* --IdentityProvider.token=3b30677a66c7a27927bb717f152b990f18bf8998c9644b1f" --nodes=1 --cpus-per-task=6 --mem=200G --time=02:30:00 --qos="job_gratis" $@
+
+port=16805
+
+srun --nodes=1 --cpus-per-task=2 --mem=20G --time=00:30:00 --qos="job_gratis" bash jupyter_remote_port_forward.sh $port --ServerApp.allow_origin=* --IdentityProvider.token=3b30677a66c7a27927bb717f152b990f18bf8998c9644b1f
+
