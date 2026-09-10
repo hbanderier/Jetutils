9f7bed3f7f630f31e2730190ed6309d591846792
Hugo <hugo.banderier@giub.unibe.ch>
2026-09-10 08:57:43 +0200

    era5 wind data during spells

--- scripts/Jupyter/launch_jupyter.sh
+++ scripts/Jupyter/launch_jupyter.sh
@@ -2,5 +2,5 @@
 
 port=16805
 
-srun --nodes=1 --cpus-per-task=2 --mem=20G --time=00:30:00 --qos="job_gratis" bash jupyter_remote_port_forward.sh $port --ServerApp.allow_origin=* --IdentityProvider.token=3b30677a66c7a27927bb717f152b990f18bf8998c9644b1f
+srun --nodes=1 --cpus-per-task=2 --mem=200G --time=00:40:00 --qos="job_gratis" bash jupyter_remote_port_forward.sh $port --ServerApp.allow_origin=* --IdentityProvider.token=3b30677a66c7a27927bb717f152b990f18bf8998c9644b1f
 
